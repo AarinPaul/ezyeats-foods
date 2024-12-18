165f70af12a09c6211597b6ce95fea9990f01d6c
AarinPaul <paulaarin2005@gmail.com>
2024-12-18 23:59:23 +0530

    changes commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ import LoginPopup from './components/LoginPopup/LoginPopup'
 import Home from './pages/Home/Home';
 import Cart from './pages/Cart/Cart';
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder';
-import Verify from './pages/Verify/verify';
+import verify from './pages/Verify/Verify'
 import MyOrders from './pages/MyOrders/MyOrders'
 
 const App = () => {
@@ -24,7 +24,7 @@ const App = () => {
           <Route path='/' element={<Home/>} />
           <Route path='/cart' element={<Cart/>} />
           <Route path='/order' element={<PlaceOrder/>} />
-          <Route path='/verify' element={<Verify/>} />
+          <Route path='/verify' element={<verify/>} />
           <Route path='/myorders' element={<MyOrders/>} />
         </Routes>
       </div>
